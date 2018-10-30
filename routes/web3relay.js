fd09dd540c2efe2e248c93e843cc45df41a643c2
Unknown <mail@mathyn.com>
2018-10-30 14:15:19 +0100

    Config is now read properly

--- routes/web3relay.js
+++ routes/web3relay.js
@@ -15,7 +15,47 @@ var getLatestBlocks = require('./index').getLatestBlocks;
 var filterBlocks = require('./filters').filterBlocks;
 var filterTrace = require('./filters').filterTrace;
 
-var config = JSON.parse(fs.readFileSync("./config.json"));
+// Try and read config
+var config = {};
+
+try {
+    var configContents = fs.readFileSync('config.json');
+    config = JSON.parse(configContents);
+}
+catch (error) {
+    if (error.code === 'ENOENT') {
+        console.log('No config file found. Using default configuration (will ' + 
+            'download all blocks starting from latest)');
+    }
+    else {
+        throw error;
+        process.exit(1);
+    }
+}
+
+// set the default geth address if it's not provided
+if (!('gethAddress' in config) || (typeof config.gethAddress) !== 'string') {
+    config.gethAddress = "localhost"; // default
+}
+
+// set the default geth port if it's not provided
+if (!('gethPort' in config) || (typeof config.gethPort) !== 'number') {
+    config.gethPort = 8545; // default
+}
+
+// set the default output directory if it's not provided
+if (!('output' in config) || (typeof config.output) !== 'string') {
+    config.output = '.'; // default this directory
+}
+
+// set the default blocks if it's not provided
+if (!('blocks' in config) || !(Array.isArray(config.blocks))) {
+    config.blocks = [];
+    config.blocks.push({'start': 0, 'end': 'latest'});
+}
+
+console.log('Using configuration:');
+console.log(config);
 
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
