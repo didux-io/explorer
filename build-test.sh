75bc83995797b9d5066e8b1a7d1b43531c516f1e
ElkanRoelen <roelen@elkan.nl>
2022-09-30 14:03:14 +0200

    Clean up repo

--- build-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $(aws ecr get-login --no-include-email --region eu-central-1)
-
-docker build --no-cache -t didux/explorer:test .
-docker tag didux/explorer:test 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/explorer:test
-docker push 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/explorer:test
